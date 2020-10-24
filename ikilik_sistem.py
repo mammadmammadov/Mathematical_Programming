0c7e2442d53eab0343432fdfbfc7aa6b58aa0409
mammadmammadov <73391896+mammadmammadov@users.noreply.github.com>
2020-10-24 18:04:50 +0400

    Add files via upload

---
+++ ikilik_sistem.py
@@ -0,0 +1,22 @@
+def convertToDecimal(number):
+    numberInString=str(number)
+    reversedNumberInString=numberInString[::-1]
+    valueInDecimal=0
+    lengthOfNumberInString=len(numberInString)
+    for digit in range(lengthOfNumberInString):
+        valueInDecimal =valueInDecimal+int(reversedNumberInString[digit])*2**digit
+    return valueInDecimal
+def convertToBinary(number):
+    valueInBinary=""
+    while number>=1:
+        valueInBinary=valueInBinary+str(number%2)
+        number=number//2
+        if number==1:
+            valueInBinary=valueInBinary+"1"
+            break
+    return valueInBinary[::-1]
+number1=int(input("Input the first number: "))
+number2=int(input("Input the second number: "))
+print(f"Multiply of these numbers")
+
+
